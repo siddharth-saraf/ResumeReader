71e9e74c300c04ea111639ba5e5829ae11d10f87
Siddharth Saraf <saraf.siddharth@gmail.com>
2024-11-08 14:40:41 -0800

    Updated to have working basic functionality

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import pytesseract
 from PIL import Image
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import pdfplumber
 
 # Load environment variables
 load_dotenv()
@@ -11,39 +11,41 @@ load_dotenv()
 # Configure Gemini API
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-def perform_ocr(image):
-    """Perform OCR on the uploaded image."""
-    text = pytesseract.image_to_string(image)
-    return text
 
-def process_with_gemini(text):
+def extract_data(uploaded_file):
+    data = []
+    with pdfplumber.open(uploaded_file) as pdf:
+        pages = pdf.pages
+        for page in pages:
+            data.append(page.extract_text())
+    return data
+
+def process_with_gemini(text, user_prompt):
     """Process the extracted text with Gemini API."""
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(f"Analyze the following resume extracted from an image highlighting the key skils and giving a short summary and rating and areas for improvement: {text}")
+    response = model.generate_content(f"Answer the following prompt as a recruiter for a high tech company for a college student at the University of Washington: {text}. For the following resume: {user_prompt}")
     return response.text
 
 def main():
     st.title("OCR and Gemini Text Analysis App")
 
     # File uploader
-    uploaded_file = st.file_uploader("Choose an image file", type=["png", "jpg", "jpeg"])
+    uploaded_file = st.file_uploader("Choose a pdf file", type=["pdf"])
 
     if uploaded_file is not None:
-        # Display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        # Extract data
+        extracted_text = extract_data(uploaded_file)
+
+        # Get User Prompt
+        user_prompt = st.text_input("Enter prompt")
 
         # Perform OCR
-        if st.button("Perform OCR and Analyze"):
-            with st.spinner("Processing..."):
-                # OCR
-                extracted_text = perform_ocr(image)
-                st.subheader("Text Extracted")
-
-                # Gemini Analysis
-                analysis = process_with_gemini(extracted_text)
+        if st.button("Submit"):
+            with st.spinner("Processing"):
+                analysis = process_with_gemini(extracted_text, user_prompt)
                 st.subheader("Gemini Analysis:")
                 st.write(analysis)
+
 
 if __name__ == "__main__":
     main()
